6af805e8d479fd6c7a5ae564489b6d3412b4c3a0
Joris Ooms <info@jorisooms.be>
2017-02-13 15:42:33 +0100

    update

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cheerio = require('cheerio');
 const _ = require('lodash');
 
 const URL = 'http://www.metsense.be/nl/onze-uitbatingen';
+const BOT_TRIGGERS = ['direct_message', 'direct_mention'];
 
 const controller = Botkit.slackbot({
   debug: false,
@@ -25,24 +26,23 @@ const weekMenu = {
 
 const parseHtml = html => (
   new Promise((resolve) => {
+    const firstOrLastTable = moment().week() % 2 === 0 ? 'first' : 'last';
     const $ = cheerio.load(html);
     const recipesFromNodes = $('.restaurant-menu')
       .eq(7)
-      .find('table')
-      .first()
+      .find('table')[firstOrLastTable]()
       .find('tr')
       .map((rowIndex, rowNode) => {
         if (rowIndex === 0 || rowIndex === 7) return null;
         const row = $(rowNode);
-        return row.map((cellIndex, cellNode) => {
-          const cell = $(cellNode);
-          return cell
+        return row.map((cellIndex, cellNode) => (
+          $(cellNode)
             .find('td')
             .last()
             .text()
             .replace(/&nbsp;/g, '')
-            .trim();
-        }).toArray();
+            .trim()
+        )).toArray();
       });
     const recipes = _.compact($(recipesFromNodes).toArray());
 
@@ -52,7 +52,6 @@ const parseHtml = html => (
     weekMenu[4] = recipes[3];
     weekMenu[5] = recipes[4];
     weekMenu.suggestion = recipes[5];
-
     return resolve(weekMenu);
   })
 );
@@ -61,49 +60,53 @@ const getMenu = () => (
   request(URL).then(parseHtml).catch(console.log) // eslint-disable-line
 );
 
-controller.hears('maandag', ['direct_message', 'direct_mention'], (bot, message) => {
+controller.hears('maandag', BOT_TRIGGERS, (bot, message) => {
   const weekDay = 1;
   getMenu().then(menu => (
     bot.reply(message, `Maandag eten we ${menu[weekDay].toLowerCase()}.`)
   ));
 });
 
-controller.hears('dinsdag', ['direct_message', 'direct_mention'], (bot, message) => {
+controller.hears('dinsdag', BOT_TRIGGERS, (bot, message) => {
   const weekDay = 2;
   getMenu().then(menu => (
     bot.reply(message, `Dinsdag eten we ${menu[weekDay].toLowerCase()}.`)
   ));
 });
 
-controller.hears('woensdag', ['direct_message', 'direct_mention'], (bot, message) => {
+controller.hears('woensdag', BOT_TRIGGERS, (bot, message) => {
   const weekDay = 3;
   getMenu().then(menu => (
     bot.reply(message, `Woensdag eten we ${menu[weekDay].toLowerCase()}.`)
   ));
 });
 
-controller.hears('donderdag', ['direct_message', 'direct_mention'], (bot, message) => {
+controller.hears('donderdag', BOT_TRIGGERS, (bot, message) => {
   const weekDay = 4;
   getMenu().then(menu => (
     bot.reply(message, `Donderdag eten we ${menu[weekDay].toLowerCase()}.`)
   ));
 });
 
-controller.hears('vrijdag', ['direct_message', 'direct_mention'], (bot, message) => {
+controller.hears('vrijdag', BOT_TRIGGERS, (bot, message) => {
   const weekDay = 5;
   getMenu().then(menu => (
     bot.reply(message, `Vrijdag eten we durum (of ${menu[weekDay].toLowerCase()}).`)
   ));
 });
 
-controller.hears('soep', ['direct_message', 'direct_mention'], (bot, message) => {
+controller.hears('soep', BOT_TRIGGERS, (bot, message) => {
   const weekDay = moment().day();
-  getMenu().then(menu => (
-    bot.reply(message, `De soep van de dag is ${menu[weekDay].toLowerCase()}.`)
-  ));
+  getMenu().then((menu) => {
+    if (weekDay >= 1 && weekDay <= 5) {
+      bot.reply(message, `De soep van de dag is ${menu[weekDay].toLowerCase()}.`);
+    } else {
+      bot.reply(message, 'Vandaag werk ik niet!');
+    }
+  });
 });
 
-controller.hears('morgen', ['direct_message', 'direct_mention'], (bot, message) => {
+controller.hears('morgen', BOT_TRIGGERS, (bot, message) => {
   const weekDay = moment().add(1, 'day').day();
   getMenu().then((menu) => {
     if (weekDay === 5) {
@@ -114,7 +117,7 @@ controller.hears('morgen', ['direct_message', 'direct_mention'], (bot, message) 
   });
 });
 
-controller.hears('broodje', ['direct_message', 'direct_mention'], (bot, message) => {
+controller.hears('broodje', BOT_TRIGGERS, (bot, message) => {
   getMenu().then(menu => (
     bot.reply(message, `Het broodje van de week is ${menu.suggestion.toLowerCase()}.`)
   ));
